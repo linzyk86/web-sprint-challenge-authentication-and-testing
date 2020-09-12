a486eb8ed76e9c90b0c5450a82ad74e7809ad1d1
Lindsay W <lwhite5314@gmail.com>
2020-09-11 20:41:21 -0400

    auth working

--- api/server.js
+++ api/server.js
@@ -1,18 +1,32 @@
-const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
+// const express = require('express');
+// // const cors = require('cors');
+// // const helmet = require('helmet');
 
-const authenticate = require('../auth/authenticate-middleware.js');
-const authRouter = require('../auth/auth-router.js');
-const jokesRouter = require('../jokes/jokes-router.js');
+// const authenticate = require('../auth/authenticate-middleware.js');
+// const authRouter = require('../auth/auth-router.js');
+// const jokesRouter = require('../jokes/jokes-router.js');
 
-const server = express();
+// const server = express();
 
-server.use(helmet());
-server.use(cors());
-server.use(express.json());
+// // server.use(helmet());
+// // server.use(cors());
+// server.use(express.json());
 
-server.use('/api/auth', authRouter);
-server.use('/api/jokes', authenticate, jokesRouter);
+// server.use('/api/auth', authRouter);
+// server.use('/api/jokes', authenticate, jokesRouter);
+// server.use("/hobbits", hobbitsRouter)
+
+// server.get("/", (req, res) => {
+// 	res.json({
+// 		message: "Welcome to our API",
+// 	})
+// })
+
+// server.use((err, req, res, next) => {
+// 	console.log(err)
+// 	res.status(500).json({
+// 		message: "Something went wrong",
+// 	})
+// })
 
 module.exports = server;
--- index.js
+++ index.js
@@ -1,6 +1,34 @@
-const server = require('./api/server.js');
 
+const express = require('express');
+const session = require("express-session")
+
+
+// const authenticate = require('./auth/authenticate-middleware');
+const authRouter = require('./auth/auth-router.js');
+const jokesRouter = require('./jokes/jokes-router.js');
+
+const server = express();
 const PORT = process.env.PORT || 3300;
+
+server.use(express.json());
+server.use(session({
+	resave: false, // avoid recreating sessions that have not changed
+	saveUninitialized: false, // comply with GDPR laws for setting cookies automatically
+	secret: process.env.JWT_SECRET, // cryptographically sign the cookie
+}))
+
+server.use(authRouter);
+server.use(jokesRouter);
+
+server.use((err, req, res, next) => {
+	console.log(err)
+	res.status(500).json({
+		message: "Something went wrong",
+	})
+})
+
 server.listen(PORT, () => {
   console.log(`\n=== Server listening on port ${PORT} ===\n`);
 });
+
+
